5a8afea23dcd86077d701812890648255a6bd4ed
dhJendrik <jendrik.iben@deliveryhero.com>
2021-05-07 22:45:40 +0200

    ran all linters

--- src/ask_me_anything/tensorflow_metrics.py
+++ src/ask_me_anything/tensorflow_metrics.py
@@ -1,13 +1,13 @@
+import tensorflow as tf
 from tensorboard.compat.tensorflow_stub import tensor_shape
 from tensorflow.python.framework import ops
 from tensorflow.python.keras.metrics import AUC
 from tensorflow.python.keras.utils import metrics_utils
 from tensorflow.python.ops import check_ops
 from tensorflow.python.util.tf_export import keras_export
-import tensorflow as tf
 
 
-@keras_export('keras.metrics.AUC')
+@keras_export("keras.metrics.AUC")
 class AUCWithLogits(AUC):
     def update_state(self, y_true, y_pred, sample_weight=None):
         """Accumulates confusion matrix statistics.
@@ -27,23 +27,22 @@ class AUCWithLogits(AUC):
 
         if self.multi_label or (self.label_weights is not None):
             # y_true should have shape (number of examples, number of labels).
-            shapes = [
-                (y_true, ('N', 'L'))
-            ]
+            shapes = [(y_true, ("N", "L"))]
             if self.multi_label:
                 # TP, TN, FP, and FN should all have shape
                 # (number of thresholds, number of labels).
-                shapes.extend([(self.true_positives, ('T', 'L')),
-                               (self.true_negatives, ('T', 'L')),
-                               (self.false_positives, ('T', 'L')),
-                               (self.false_negatives, ('T', 'L'))])
+                shapes.extend(
+                    [
+                        (self.true_positives, ("T", "L")),
+                        (self.true_negatives, ("T", "L")),
+                        (self.false_positives, ("T", "L")),
+                        (self.false_negatives, ("T", "L")),
+                    ]
+                )
             if self.label_weights is not None:
                 # label_weights should be of length equal to the number of labels.
-                shapes.append((self.label_weights, ('L',)))
-            deps = [
-                check_ops.assert_shapes(
-                    shapes, message='Number of labels is not consistent.')
-            ]
+                shapes.append((self.label_weights, ("L",)))
+            deps = [check_ops.assert_shapes(shapes, message="Number of labels is not consistent.")]
 
         # Only forward label_weights to update_confusion_matrix_variables when
         # multi_label is False. Otherwise the averaging of individual label AUCs is
@@ -52,18 +51,15 @@ class AUCWithLogits(AUC):
         with ops.control_dependencies(deps):
             return metrics_utils.update_confusion_matrix_variables(
                 {
-                    metrics_utils.ConfusionMatrix.TRUE_POSITIVES:
-                        self.true_positives,
-                    metrics_utils.ConfusionMatrix.TRUE_NEGATIVES:
-                        self.true_negatives,
-                    metrics_utils.ConfusionMatrix.FALSE_POSITIVES:
-                        self.false_positives,
-                    metrics_utils.ConfusionMatrix.FALSE_NEGATIVES:
-                        self.false_negatives,
+                    metrics_utils.ConfusionMatrix.TRUE_POSITIVES: self.true_positives,
+                    metrics_utils.ConfusionMatrix.TRUE_NEGATIVES: self.true_negatives,
+                    metrics_utils.ConfusionMatrix.FALSE_POSITIVES: self.false_positives,
+                    metrics_utils.ConfusionMatrix.FALSE_NEGATIVES: self.false_negatives,
                 },
                 y_true,
                 y_pred,
                 self._thresholds,
                 sample_weight=sample_weight,
                 multi_label=self.multi_label,
-                label_weights=label_weights)
+                label_weights=label_weights,
+            )
--- src/utils.py
+++ src/utils.py
@@ -6,45 +6,62 @@ BASE_DATA_DIR = "/Volumes/GoogleDrive/My Drive/Ask_me_anything/data"
 
 
 def load_german_quad_dataset(json_path):
-    dummy_dict = {key: None for key in ["answer_id", "document_id", "question_id", "text",
-                                        "answer_start", "answer_category"]}
+    dummy_dict = {
+        key: None
+        for key in [
+            "answer_id",
+            "document_id",
+            "question_id",
+            "text",
+            "answer_start",
+            "answer_category",
+        ]
+    }
     qa_json = load_json(json_path)
-    qa_df = pd.json_normalize(qa_json["data"],
-                              record_path=["paragraphs", "qas"],
-                              meta=[
-                                  ["paragraphs", "context"],
-                                  ["paragraphs", "document_id"]
-                              ])
+    qa_df = pd.json_normalize(
+        qa_json["data"],
+        record_path=["paragraphs", "qas"],
+        meta=[["paragraphs", "context"], ["paragraphs", "document_id"]],
+    )
     qa_df["answers"] = qa_df.answers.map(lambda x: x if len(x) != 0 else dummy_dict)
     answers_df = pd.concat(qa_df.answers.map(pd.json_normalize).values).reset_index(drop=True)
-    
+
     qa_df = pd.concat([qa_df, answers_df], axis=1)
-    
+
     qa_df.drop(columns=["answers", "question_id", "document_id"], inplace=True)
-    qa_df.rename(columns={
-        "paragraphs.context": "context",
-        "paragraphs.document_id": "document_id",
-        "text": "answer_text",
-        "id": "question_id"
-    }, inplace=True)
+    qa_df.rename(
+        columns={
+            "paragraphs.context": "context",
+            "paragraphs.document_id": "document_id",
+            "text": "answer_text",
+            "id": "question_id",
+        },
+        inplace=True,
+    )
     return qa_df
 
 
 def load_qa_dataset(json_path):
     qa_json = load_json(json_path)
-    qa_df = pd.json_normalize(qa_json["data"],
-                              record_path=["paragraphs", "qas", "answers"],
-                              meta=["title",
-                                    ["paragraphs", "context"],
-                                    ["paragraphs", "qas", "question"],
-                                    ["paragraphs", "qas", "id"],
-                                    ])
-    qa_df.rename(columns={
-        "paragraphs.context": "context",
-        "paragraphs.qas.question": "question",
-        "paragraphs.qas.id": "question_id",
-        "text": "answer_text"
-    }, inplace=True)
+    qa_df = pd.json_normalize(
+        qa_json["data"],
+        record_path=["paragraphs", "qas", "answers"],
+        meta=[
+            "title",
+            ["paragraphs", "context"],
+            ["paragraphs", "qas", "question"],
+            ["paragraphs", "qas", "id"],
+        ],
+    )
+    qa_df.rename(
+        columns={
+            "paragraphs.context": "context",
+            "paragraphs.qas.question": "question",
+            "paragraphs.qas.id": "question_id",
+            "text": "answer_text",
+        },
+        inplace=True,
+    )
     qa_df = qa_df[["title", "context", "question", "answer_text", "answer_start", "question_id"]]
     return qa_df
 
@@ -52,17 +69,13 @@ def load_qa_dataset(json_path):
 def convert_drp_to_ana(json_path):
     dpr_json = load_json(json_path)
     meta_columns = ["question", "answers"]
-    negatives_df = pd.json_normalize(dpr_json,
-                                     record_path=["hard_negative_ctxs"],
-                                     meta=meta_columns
-                                    )
-    positives_df = pd.json_normalize(dpr_json,
-                                     record_path=["positive_ctxs"],
-                                     meta=meta_columns
-                                    )
+    negatives_df = pd.json_normalize(
+        dpr_json, record_path=["hard_negative_ctxs"], meta=meta_columns
+    )
+    positives_df = pd.json_normalize(dpr_json, record_path=["positive_ctxs"], meta=meta_columns)
     negatives_df["contains_answer"] = False
     positives_df["contains_answer"] = True
-    
+
     ana_df = pd.concat([negatives_df, positives_df]).reset_index(drop=True)
     ana_df.rename(columns={"text": "context", "answers": "answer"}, inplace=True)
     return ana_df
